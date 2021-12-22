0e387e6bb2c5e0a4ac18a66b71628fe7feb74d7b
Baiche Ramy <66490893+BaicheRamy@users.noreply.github.com>
2021-12-22 21:02:11 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./styles.css";
+import Lottie from "react-lottie";
+import animationData from "./lotties/algeriaicon.json";
+
+export default function App() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
+  return (
+    <div>
+      <Lottie options={defaultOptions} height={400} width={400} />
+    </div>
+  );
+}
